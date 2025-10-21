62e9509f849c610ee12ea01ab8f8d253e46e3e49
Ethan Gramowski <gramo012@umn.edu>
2025-10-21 12:58:40 -0500

    rm image limit

--- app/api/PTZ/route.ts
+++ app/api/PTZ/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
     try {
         const { blobs } = await list({
-            limit: 100,
+            limit: 10000,
             prefix: 'capture_',
         });
 
